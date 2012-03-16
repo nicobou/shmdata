8926b685d19b5bc7b53a03324b3908af39cb43b3
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2012-03-16 16:02:11 -0400

    pcd writer properly using any-data-writer

--- examples/pcd_writer.cpp
+++ examples/pcd_writer.cpp
@@ -29,7 +29,7 @@
 #include <gst/gst.h>
 #include <gst/app/gstappsrc.h>
 #include <gst/app/gstappbuffer.h>
-#include "shmdata/base-writer.h"
+#include "shmdata/any-data-writer.h"
 
 
 #ifdef WIN32
@@ -41,11 +41,8 @@
 typedef struct _App App;
 struct _App
 {
-    GstElement *pipe;
-    GstElement *src;
-    GstElement *id;
     gboolean on;
-    shmdata_base_writer_t *writer;
+    shmdata_any_writer_t *writer;
 };
 
 App s_app;
@@ -54,67 +51,41 @@ App s_app;
 void
 leave(int sig) {
     s_app.on = false;
-    gst_element_set_state (s_app.pipe, GST_STATE_NULL);
-    gst_object_unref (GST_OBJECT (s_app.pipe));
-    exit(sig);
+//    exit(sig);
 }
 
 int
-main (int argc, char** argv)
+main (int argc, char *argv[])
 {
-
-    App *app = &s_app;
 
     (void) signal(SIGINT,leave);
 
-    std::string socketName;
 
     /* Check input arguments */
     if (argc != 2) {
 	g_printerr ("Usage: %s <socket-path>\n", argv[0]);
 	return -1;
     }
-    socketName.append (argv[1]);
     
-    gst_init (&argc, &argv);
+    //shmdata 
+    s_app.writer = shmdata_any_writer_init ();
+    shmdata_any_writer_set_debug (s_app.writer, SHMDATA_ENABLE_DEBUG);
+    shmdata_any_writer_set_data_type(s_app.writer, "application/pcd_");
+    shmdata_any_writer_start (s_app.writer,argv[1]);
 
-    app->pipe = gst_pipeline_new (NULL);
-    g_assert (app->pipe);
-
-    app->src = gst_element_factory_make ("appsrc", NULL);
-    g_assert (app->src);
-    gst_bin_add (GST_BIN (app->pipe), app->src);
-
-    GstCaps *mycaps = gst_caps_new_simple ("application/scenicdata_", NULL);
-    gst_app_src_set_caps (GST_APP_SRC(app->src), mycaps);
-    //unref ?
-
-    app->id = gst_element_factory_make ("identity", NULL);
-    g_assert (app->id);
-    gst_bin_add (GST_BIN (app->pipe), app->id);
-
-    app->writer = shmdata_base_writer_init (socketName.c_str(),app->pipe,app->id);
-    gst_element_link (app->src, app->id);
-
-    app->on = true;
-    gst_element_set_state (app->pipe, GST_STATE_PLAYING);
-
+    //---- reading a pcd file 
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-    
-    if (pcl::io::loadPCDFile<pcl::PointXYZRGB> ("test_pcd.pcd", *cloud) == -1) //* load the file
+    //loading the cloud
+    if (pcl::io::loadPCDFile<pcl::PointXYZRGB> ("test_pcd.pcd", *cloud) == -1) 
     {
 	PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
 	return (-1);
     }
     
-//    SimpleOpenNIViewer v;
-//    v.run (cloud);
-    
+    //---- compression
     pcl::octree::PointCloudCompression<pcl::PointXYZRGB>* PointCloudEncoder;
-
-
-    // for a full list of profiles see: /io/include/pcl/compression/compression_profiles.h
-    //tunning compression
+    //tunning compression for rtp streaming: 
+    //     require iFrameRate to 0, otherwise the remote decoder is freezing 
     bool showStatistics = false;
     double pointResolution = 0.0001;
     double octreeResolution = 0.01;
@@ -122,34 +93,39 @@ main (int argc, char** argv)
     unsigned int iFrameRate = 0;
     bool doColorEncoding = true;
     unsigned int colorBitResolution = 8;
-
-    //pcl::octree::compression_Profiles_e compressionProfile = pcl::octree::HIGH_RES_ONLINE_COMPRESSION_WITH_COLOR;//LOW_RES_ONLINE_COMPRESSION_WITH_COLOR;//MED_RES_ONLINE_COMPRESSION_WITH_COLOR;
-    
     // instantiate point cloud compression for encoding and decoding
-    //PointCloudEncoder = new pcl::octree::PointCloudCompression<pcl::PointXYZRGB> (compressionProfile, true);
-    PointCloudEncoder = new pcl::octree::PointCloudCompression<pcl::PointXYZRGB> (pcl::octree::MANUAL_CONFIGURATION, showStatistics, pointResolution, octreeResolution, doVoxelGridDownDownSampling, iFrameRate, doColorEncoding, colorBitResolution);
-    // output pointcloud
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloudOut (new pcl::PointCloud<pcl::PointXYZRGB> ());
-    
+    PointCloudEncoder = new pcl::octree::PointCloudCompression<pcl::PointXYZRGB> (
+	pcl::octree::MANUAL_CONFIGURATION,
+	showStatistics, 
+	pointResolution, 
+	octreeResolution, 
+	doVoxelGridDownDownSampling, 
+	iFrameRate, doColorEncoding, 
+	colorBitResolution);
 
-    int mytime=0;
-    
     char databuf [500000];
-    int usecPeriod=30000;
+    unsigned long long myclock=0;
+    unsigned long long nsecPeriod=30000000;
 
-    while (app->on){  
+    s_app.on = true;
+
+    while (s_app.on){  
 	// stringstream to store compressed point cloud
 	std::stringstream compressedData;
-
 	PointCloudEncoder->encodePointCloud (cloud, compressedData);
-
+	
 	compressedData.read (databuf,500000);
 	int size = compressedData.gcount ();
-	GstBuffer *gstbuf = gst_app_buffer_new (databuf, size , NULL, NULL);
-	GST_BUFFER_TIMESTAMP(gstbuf) = (GstClockTime)((mytime) * 1e9/usecPeriod); 
-	gst_app_src_push_buffer (GST_APP_SRC (app->src), gstbuf);
-	usleep (usecPeriod);
-	mytime++;
+
+	shmdata_any_writer_push_data (s_app.writer,
+				      databuf, 
+				      size,
+				      myclock,
+				      NULL,
+				      NULL);
+	//clock
+	usleep (nsecPeriod/1000);
+	myclock += nsecPeriod;
     }
 
     return (0);
