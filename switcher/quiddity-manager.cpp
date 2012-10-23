8caa75479240cdb11b4c1a3775a168d8fd425562
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2012-10-23 17:25:01 -0400

    addint manager name to print

--- switcher/quiddity-manager.cpp
+++ switcher/quiddity-manager.cpp
@@ -46,7 +46,7 @@ namespace switcher
     clear_command_sync ();
     //FIXME count instances and do quit main loop
     //g_main_loop_quit (mainloop_);
-    g_print ("base quiddity manager destructed\n");
+    g_print ("base quiddity manager destructed %s\n",name_.c_str ());
   }
 
   std::string 
