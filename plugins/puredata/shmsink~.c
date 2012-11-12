a89179dc35f8fa7aaeca1712800afc49a1b7a5df
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2012-11-11 23:14:28 -0500

    adding basic push of data

--- plugins/puredata/shmsink~.c
+++ plugins/puredata/shmsink~.c
@@ -13,6 +13,8 @@
  */
 
 #include "m_pd.h"
+#include "shmdata/any-data-writer.h"
+
 #ifdef NT
 #pragma warning( disable : 4244 )
 #pragma warning( disable : 4305 )
@@ -20,14 +22,15 @@
 
 /* ------------------------ shmsink~ ----------------------------- */
 
-/* tilde object to take absolute value. */
 
 static t_class *shmsink_class;
 
 typedef struct _shmsink
 {
-    t_object x_obj; 	/* obligatory header */
-    t_float x_f;    	/* place to hold inlet's value if it's set by message */
+  t_object x_obj; 	/* obligatory header */
+  shmdata_any_writer_t *x_writer;
+  unsigned long long x_clock;
+  t_float x_f;    	/* place to hold inlet's value if it's set by message */
 } t_shmsink;
 
     /* this is the actual performance routine which acts on the samples.
@@ -38,14 +41,27 @@ typedef struct _shmsink
     and w[3] is the number of points to calculate. */
 static t_int *shmsink_perform(t_int *w)
 {
-    t_float *in = (t_float *)(w[1]);
-    t_float *out = (t_float *)(w[2]);
-    int n = (int)(w[3]);
-    while (n--)
-    {
-    	float f = *(in++);
-	*out++ = (f > 0 ? f : -f);
-    }
+   t_shmsink *x = ( t_shmsink *)(w[1]);
+  t_float *in = (t_float *)(w[2]);
+  int n = (int)(w[3]);
+  
+  shmdata_any_writer_push_data (x->x_writer,
+				in, 
+				n*sizeof (t_float), 
+				x->x_clock, 
+				NULL, 
+				NULL);
+  
+  x->x_clock += n;
+
+    /* t_float *in = (t_float *)(w[1]); */
+    /* t_float *out = (t_float *)(w[2]); */
+    /* int n = (int)(w[3]); */
+    /* while (n--) */
+    /* { */
+    /* 	float f = *(in++); */
+    /* 	*out++ = (f > 0 ? f : -f); */
+    /* } */
     return (w+4);
 }
 
@@ -54,14 +70,23 @@ static t_int *shmsink_perform(t_int *w)
     out the samples. */
 static void shmsink_dsp(t_shmsink *x, t_signal **sp)
 {
-    dsp_add(shmsink_perform, 3, sp[0]->s_vec, sp[1]->s_vec, sp[0]->s_n);
+  dsp_add(shmsink_perform, 3, x, sp[0]->s_vec, sp[0]->s_n);
 }
 
 static void *shmsink_new(void)
 {
     t_shmsink *x = (t_shmsink *)pd_new(shmsink_class);
-    outlet_new(&x->x_obj, gensym("signal"));
+    //outlet_new(&x->x_obj, gensym("signal"));
     x->x_f = 0;
+    x->x_clock = 0;
+    x->x_writer = shmdata_any_writer_init ();
+    /* if (verbose) */
+    /*    shmdata_any_writer_set_debug (s_app.writer, SHMDATA_ENABLE_DEBUG); */
+    /*  else */
+    /*    shmdata_any_writer_set_debug (s_app.writer, SHMDATA_DISABLE_DEBUG); */
+    shmdata_any_writer_set_data_type (x->x_writer, "audio/x-raw-float");
+    shmdata_any_writer_set_path (x->x_writer,"/tmp/pd-shmdata-test");
+    shmdata_any_writer_start (x->x_writer);
     return (x);
 }
 
