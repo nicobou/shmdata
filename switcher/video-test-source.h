c8ecb42015ceef20eba2b860d734d93ee7c2bb54
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2013-01-23 10:38:21 -0500

    update license with 2013

--- switcher/video-test-source.h
+++ switcher/video-test-source.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Nicolas Bouillot (http://www.nicolasbouillot.net)
+ * Copyright (C) 2012-2013 Nicolas Bouillot (http://www.nicolasbouillot.net)
  *
  * This file is part of switcher.
  *
