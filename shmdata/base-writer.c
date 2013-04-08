676f39adff95c11a53d2aa54c8660070e22125ad
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2013-04-08 10:06:20 -0400

    same in writer

--- shmdata/base-writer.c
+++ shmdata/base-writer.c
@@ -55,7 +55,8 @@ shmdata_base_writer_clean_element (GstElement *element)
 	gst_iterator_free (pad_iter);
 	if (GST_STATE_TARGET (element) != GST_STATE_NULL)
 	  if (GST_STATE_CHANGE_ASYNC == gst_element_set_state (element, GST_STATE_NULL))
-	    gst_element_get_state (element, NULL, NULL, GST_CLOCK_TIME_NONE);//warning this may be blocking
+	    while (GST_STATE (element) != GST_STATE_NULL)
+	      gst_element_get_state (element, NULL, NULL, GST_CLOCK_TIME_NONE);//warning this may be blocking
 	if (GST_IS_BIN (gst_element_get_parent (element)))
 	  gst_bin_remove (GST_BIN (gst_element_get_parent (element)), element);
       }
