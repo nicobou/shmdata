a4b93866c007a502780efc058384a308ec1e2c62
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2013-04-23 08:37:29 -0400

    progress on signals

--- switcher/signal-string.cpp
+++ switcher/signal-string.cpp
@@ -41,7 +41,7 @@ namespace switcher
       }
      
     guint id = g_signal_lookup (signame.c_str (), 
-				G_OBJECT_CLASS_TYPE(object));
+				G_TYPE_FROM_CLASS (G_OBJECT_CLASS_TYPE(object)));
     if (id == 0)
       {
 	g_debug ("Signal: object does not have a signal named %s", 
@@ -70,91 +70,86 @@ namespace switcher
   void
   Signal::make_description ()
   {
-  //   /* Signals/Actions Block */
-  //   guint *signals;
-  //   guint nsignals;
-  //   gint i = 0, j, k;
-  //   GSignalQuery *query = NULL;
-  //   GType type;
-  //   GSList *found_signals, *l;
-     
+    /* Signals/Actions Block */
+    guint *signals;
+    guint nsignals;
+    gint i = 0, j, k;
+    GSignalQuery *query = NULL;
+    GType type;
+    GSList *found_signals, *l;
+    
+    
+    // for (type = G_OBJECT_TYPE (element); type; type = g_type_parent (type)) {
+    //   if (type == GST_TYPE_ELEMENT || type == GST_TYPE_OBJECT)
+    // 	break;
       
-  //   for (type = G_OBJECT_TYPE (element); type; type = g_type_parent (type)) {
-  //     if (type == GST_TYPE_ELEMENT || type == GST_TYPE_OBJECT)
-  //       break;
+    //   if (type == GST_TYPE_BIN && G_OBJECT_TYPE (element) != GST_TYPE_BIN)
+    //      continue;
 
-  //     if (type == GST_TYPE_BIN && G_OBJECT_TYPE (element) != GST_TYPE_BIN)
-  //       continue;
+    // signals = g_signal_list_ids (type, &nsignals);
+    // for (i = 0; i < nsignals; i++) {
+    query = g_new0 (GSignalQuery, 1);
+    g_signal_query (id_, query);
+    
+    // if ((k == 0 && !(query->signal_flags & G_SIGNAL_ACTION)) ||
+    // 	(k == 1 && (query->signal_flags & G_SIGNAL_ACTION)))
+    if (query->signal_flags & G_SIGNAL_ACTION)
+      g_print ("-action-\n");
+    else
+      g_print ("-signal-\n");
+    found_signals = g_slist_append (found_signals, query);
+    // else
+    //   g_free (query);
+    // }
+    // g_free (signals);
+    //      signals = NULL;
+    //    }
 
-  //     signals = g_signal_list_ids (type, &nsignals);
-  //     for (i = 0; i < nsignals; i++) {
-  //       query = g_new0 (GSignalQuery, 1);
-  //       g_signal_query (signals[i], query);
+    // if (found_signals) {
+    //   n_print ("\n");
+    //   if (k == 0)
+    //     n_print ("Element Signals:\n");
+    //   else
+    //     n_print ("Element Actions:\n");
+    // } else {
+    //   continue;
+    // }
 
-  //       if ((k == 0 && !(query->signal_flags & G_SIGNAL_ACTION)) ||
-  //           (k == 1 && (query->signal_flags & G_SIGNAL_ACTION)))
-  //         found_signals = g_slist_append (found_signals, query);
-  //       else
-  //         g_free (query);
-  //     }
-  //     g_free (signals);
-  //     signals = NULL;
-  //   }
+    for (l = found_signals; l; l = l->next) {
+      // gchar *indent;
+      // int indent_len;
 
-  //   if (found_signals) {
-  //     n_print ("\n");
-  //     if (k == 0)
-  //       n_print ("Element Signals:\n");
-  //     else
-  //       n_print ("Element Actions:\n");
-  //   } else {
-  //     continue;
-  //   }
-
-  //   for (l = found_signals; l; l = l->next) {
-  //     gchar *indent;
-  //     int indent_len;
-
-  //     query = (GSignalQuery *) l->data;
-  //     indent_len = strlen (query->signal_name) +
-  // 	strlen (g_type_name (query->return_type)) + 24;
-
-  //     indent = g_new0 (gchar, indent_len + 1);
-  //     memset (indent, ' ', indent_len);
-
-  //     n_print ("  \"%s\" :  %s user_function (%s* object",
-  // 	       query->signal_name,
-  // 	       g_type_name (query->return_type), g_type_name (type));
-
-  //     for (j = 0; j < query->n_params; j++) {
-  //       if (_name)
-  //         g_print ("%s", _name);
-  //       if (G_TYPE_IS_FUNDAMENTAL (query->param_types[j])) {
-  //         g_print (",\n%s%s arg%d", indent,
-  // 		   g_type_name (query->param_types[j]), j);
-  //       } else if (G_TYPE_IS_ENUM (query->param_types[j])) {
-  //         g_print (",\n%s%s arg%d", indent,
-  // 		   g_type_name (query->param_types[j]), j);
-  //       } else {
-  //         g_print (",\n%s%s* arg%d", indent,
-  // 		   g_type_name (query->param_types[j]), j);
-  //       }
-  //     }
-
-  //     if (k == 0) {
-  //       if (_name)
-  //         g_print ("%s", _name);
-  //       g_print (",\n%sgpointer user_data);\n", indent);
-  //     } else
-  //       g_print (");\n");
-
-  //     g_free (indent);
-  //   }
-
-  //   if (found_signals) {
-  //     g_slist_foreach (found_signals, (GFunc) g_free, NULL);
-  //     g_slist_free (found_signals);
-  //   }
+      query = (GSignalQuery *) l->data;
+      // indent_len = strlen (query->signal_name) +
+      // 	strlen (g_type_name (query->return_type)) + 24;
+       
+      // indent = g_new0 (gchar, indent_len + 1);
+      // memset (indent, ' ', indent_len);
+       
+      g_print ("  \"%s\" :  %s user_function (%s* object",
+	       query->signal_name,
+	       g_type_name (query->return_type), g_type_name (type));
+       
+      for (j = 0; j < query->n_params; j++) {
+	if (G_TYPE_IS_FUNDAMENTAL (query->param_types[j])) {
+	  g_print (",\n%s arg%d",
+		   g_type_name (query->param_types[j]), j);
+	} else if (G_TYPE_IS_ENUM (query->param_types[j])) {
+	  g_print (",\n%s arg%d",
+		   g_type_name (query->param_types[j]), j);
+	} else {
+	  g_print (",\n%s* arg%d",
+		   g_type_name (query->param_types[j]), j);
+	}
+      }
+       
+      g_print ("\n");
+    }
+    
+    if (found_signals) {
+      g_slist_foreach (found_signals, (GFunc) g_free, NULL);
+      g_slist_free (found_signals);
+    }
   }
 }
- 
+
