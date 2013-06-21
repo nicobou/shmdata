ff6f36fa2ff4bdc40d3371a425021e60e0edf83b
Mike Wozniewski <mike@mikewoz.com>
2013-06-21 16:01:16 -0400

    Initial XCode project for switcher

---
+++ xcode/src/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  Switcher
+//
+//  Created by Mike Wozniewski on 13-06-21.
+//  Copyright (c) 2013 SAT. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Foundation/NSFileHandle.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+{
+    NSPipe *pipe;
+    NSFileHandle *pipeReadHandle;
+    IBOutlet NSTextView *textView;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+
+- (void)logPrint:(NSString*)s;
+
+@end
