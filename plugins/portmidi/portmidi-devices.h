e5a26b126542ed38db356968289fa0bbe9f827c8
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2013-12-17 13:30:30 -0500

    fix portmidi mutex issue

--- plugins/portmidi/portmidi-devices.h
+++ plugins/portmidi/portmidi-devices.h
@@ -89,7 +89,7 @@ namespace switcher
       
     private:
       GMutex streams_mutex_;
-      GMutex finalize_mutex_;
+      static GMutex finalize_mutex_;
       gboolean finalizing_;
       std::map<PmStream *, std::pair<on_pm_event_method, void *> > input_callbacks_;
       std::map<PmStream *, std::queue<PmEvent> *> output_queues_;
