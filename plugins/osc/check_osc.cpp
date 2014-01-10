39b65788810aeef0cbc84df35afc356640097dbf
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2014-01-10 18:34:33 -0500

    starting osc-to-prop

---
+++ plugins/osc/check_osc.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2012-2013 Nicolas Bouillot (http://www.nicolasbouillot.net)
+ *
+ * This file is part of switcher-myplugin.
+ *
+ * libswitcher is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "switcher/quiddity-manager.h"
+#include "switcher/quiddity-basic-test.h"
+#include <vector>
+#include <string>
+#include <iostream>
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+int
+main ()
+{
+   bool success = true;
+
+   {
+  switcher::QuiddityManager::ptr manager = switcher::QuiddityManager::make_manager("test_manager");  
+
+  #ifdef HAVE_CONFIG_H
+      gchar *usr_plugin_dir = g_strdup_printf ("./%s", LT_OBJDIR);
+      manager->scan_directory_for_plugins (usr_plugin_dir);
+      g_free (usr_plugin_dir);
+  #else
+      return 1;
+  #endif
+
+      if (!switcher::QuiddityBasicTest::test_full (manager, "OSCctl"))
+        success = false;
+
+      if (!switcher::QuiddityBasicTest::test_full (manager, "OSCprop"))
+        success = false;
+
+   }//end of scope is releasing the manager
+
+   if (success)
+     return 0;
+   else
+     return 1;
+}
+
+
+
