d486377ba2d406188497764393a54d9f34d8395d
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2014-06-13 16:45:37 -0400

    set buffer-time in jacksink

--- switcher/jack-sink.cpp
+++ switcher/jack-sink.cpp
@@ -77,7 +77,7 @@ namespace switcher
   {
     GError *error = NULL;
 
-    gchar *description = g_strdup_printf ("audioconvert ! audioresample ! jackaudiosink provide-clock=false slave-method=resample max-lateness=100000 client-name=%s sync=false", client_name_);
+    gchar *description = g_strdup_printf ("audioconvert ! audioresample ! jackaudiosink provide-clock=false slave-method=resample buffer-time=20000 max-lateness=100000 client-name=%s sync=false", client_name_);
     
     jacksink_ = gst_parse_bin_from_description (description,
 						TRUE,
