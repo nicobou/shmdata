f00b2b2221ebea918bafb5c6444c2a520e5c5b8c
Emmanuel Durand <emmanueldurand@gmail.com>
2014-08-04 17:35:59 -0400

    Shmdata writer in Python3 wrapper now works

--- wrappers/python/pyshmdata.cpp
+++ wrappers/python/pyshmdata.cpp
@@ -14,13 +14,17 @@
 
 #include "pyshmdata.h"
 
+#include <string>
+
+using namespace std;
+
 /*************/
+// Any-data-writer
 PyMODINIT_FUNC
 PyInit_pyshmdata(void)
 {
     PyObject* m;
 
-    pyshmdata_WriterType.tp_new = PyType_GenericNew;
     if (PyType_Ready(&pyshmdata_WriterType) < 0)
         return NULL;
 
@@ -34,11 +38,118 @@ PyInit_pyshmdata(void)
 }
 
 /*************/
-static PyObject*
-shmdata_writer_init(PyObject* self, PyObject* args)
+void
+Writer_dealloc(pyshmdata_WriterObject* self)
 {
-    const char* filename;
+    Py_XDECREF(self->path);
+    Py_XDECREF(self->datatype);
+    if (self->writer != NULL)
+        shmdata_any_writer_close(self->writer);
 
-    if (!PyArg_ParseTuple(args, "s", &filename))
-        return NULL;
+    Py_TYPE(self)->tp_free((PyObject*)self);
 }
+
+/*************/
+PyObject*
+Writer_new(PyTypeObject* type, PyObject* args, PyObject* kwds)
+{
+    pyshmdata_WriterObject* self;
+
+    self = (pyshmdata_WriterObject*)type->tp_alloc(type, 0);
+    if (self != NULL) {
+        self->path = PyUnicode_FromString("");
+        if (self->path == NULL) {
+            Py_DECREF(self);
+            return NULL;
+        }
+
+        self->datatype = PyUnicode_FromString("");
+        if (self->datatype == NULL) {
+            Py_DECREF(self);
+            return NULL;
+        }
+    }
+
+    return (PyObject*)self;
+}
+
+/*************/
+int
+Writer_init(pyshmdata_WriterObject* self, PyObject* args, PyObject* kwds)
+{
+    PyObject *path = NULL;
+    PyObject *datatype = NULL;
+    PyObject *tmp = NULL;
+
+    static char *kwlist[] = {(char*)"path", (char*)"datatype", NULL};
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "|OO", kwlist,
+                                     &path, &datatype))
+        return -1;
+
+    if (path) {
+        tmp = self->path;
+        Py_INCREF(path);
+        self->path = path;
+        Py_XDECREF(tmp);
+    }
+
+    if (datatype) {
+        tmp = self->datatype;
+        Py_INCREF(datatype);
+        self->datatype = datatype;
+        Py_XDECREF(tmp);
+    }
+
+    return 0;
+}
+
+/*************/
+PyObject*
+Writer_push(pyshmdata_WriterObject* self, PyObject* args)
+{
+    PyObject* buffer = NULL;
+    unsigned long long timestamp = 0;
+
+    if (!PyArg_ParseTuple(args, "O|K", &buffer, &timestamp)) {
+        return NULL;
+    }
+
+    if (!PyObject_TypeCheck(buffer, &PyByteArray_Type)) {
+        return NULL;
+    }
+
+    if (self->writer == NULL) {
+        string path(PyUnicode_AsUTF8(self->path));
+        string datatype(PyUnicode_AsUTF8(self->datatype));
+
+        self->writer = shmdata_any_writer_init();
+        if (self->writer == NULL) {
+            return NULL;
+        }
+
+        shmdata_any_writer_set_data_type(self->writer, datatype.c_str());
+        if (!shmdata_any_writer_set_path(self->writer, path.c_str())) {
+            return NULL;
+        }
+
+        shmdata_any_writer_start(self->writer);
+    }
+
+    if (self->writer != NULL) {
+        Py_INCREF(buffer);
+        shmdata_any_writer_push_data(self->writer, (void*)PyByteArray_AsString(buffer), PyByteArray_Size(buffer), timestamp, Writer_freeObject, buffer);
+    }
+
+    return Py_BuildValue("i", 1);
+}
+
+/*************/
+void
+Writer_freeObject(void* user_data)
+{
+    PyObject* buffer = (PyObject*)user_data;
+    Py_DECREF(buffer);
+}
+
+/*************/
+// Any-data-reader
--- wrappers/python/pyshmdata.h
+++ wrappers/python/pyshmdata.h
@@ -19,6 +19,7 @@
 #include "any-data-writer.h"
 
 #include <Python.h>
+#include "structmember.h"
 
 #ifdef __cplusplush
 extern "C"
@@ -29,15 +30,34 @@ extern "C"
 // Any-data-writer
 typedef struct {
     PyObject_HEAD
-    shmdata_any_writer_t* writer;
+    PyObject* path {NULL};
+    PyObject* datatype {NULL};
+    shmdata_any_writer_t* writer {NULL};
 } pyshmdata_WriterObject;
+
+static void Writer_dealloc(pyshmdata_WriterObject* self);
+static PyObject* Writer_new(PyTypeObject* type, PyObject* args, PyObject* kwds);
+static int Writer_init(pyshmdata_WriterObject* self, PyObject* args, PyObject* kwds);
+static PyObject* Writer_push(pyshmdata_WriterObject* self, PyObject* args);
+static void Writer_freeObject(void* user_data);
+
+static PyMemberDef Writer_members[] = {
+    {(char*)"path", T_OBJECT_EX, offsetof(pyshmdata_WriterObject, path), 0, (char*)"Path to the shmdata output"},
+    {(char*)"datatype", T_OBJECT_EX, offsetof(pyshmdata_WriterObject, datatype), 0, (char*)"Type of the data sent"},
+    {NULL}
+};
+
+static PyMethodDef Writer_methods[] = {
+    {(char*)"push", (PyCFunction)Writer_push, METH_VARARGS, (char*)"Push data through shmdata"},
+    {NULL}
+};
 
 static PyTypeObject pyshmdata_WriterType = {
     PyVarObject_HEAD_INIT(NULL, 0)
-    "pyshmdata.Writer",                 /* tp_name */
+    (char*)"pyshmdata.Writer",          /* tp_name */
     sizeof(pyshmdata_WriterObject),     /* tp_basicsize */
     0,                                  /* tp_itemsize */
-    0,                                  /* tp_dealloc */
+    (destructor)Writer_dealloc,         /* tp_dealloc */
     0,                                  /* tp_print */
     0,                                  /* tp_getattr */
     0,                                  /* tp_setattr */
@@ -52,19 +72,27 @@ static PyTypeObject pyshmdata_WriterType = {
     0,                                  /* tp_getattro */
     0,                                  /* tp_setattro */
     0,                                  /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT,                 /* tp_flags */
-    "PyShmdata Writer Object",          /* tp_doc */
+    Py_TPFLAGS_DEFAULT
+        | Py_TPFLAGS_BASETYPE,          /* tp_flags */
+    (char*)"PyShmdata Writer Object",   /* tp_doc */
+    0,                                  /* tp_traverse */
+    0,                                  /* tp_clear */
+    0,                                  /* tp_richcompare */
+    0,                                  /* tp_weaklistoffset */
+    0,                                  /* tp_iter */
+    0,                                  /* tp_iternext */
+    Writer_methods,                     /* tp_methods */
+    Writer_members,                     /* tp_members */
+    0,                                  /* tp_getset */
+    0,                                  /* tp_base */
+    0,                                  /* tp_dict */
+    0,                                  /* tp_descr_get */
+    0,                                  /* tp_descr_set */
+    0,                                  /* tp_dictoffset */
+    (initproc)Writer_init,              /* tp_init */
+    0,                                  /* tp_alloc */
+    Writer_new                          /* tp_new */
 };
-
-static PyModuleDef pyshmdatamodule = {
-    PyModuleDef_HEAD_INIT,
-    "pyshmdata",
-    "Shmdata module",
-    -1,
-    NULL, NULL, NULL, NULL, NULL
-};
-
-PyMODINIT_FUNC PyInit_pyshmdata(void);
 
 /*************/
 // Any-data-reader
@@ -74,7 +102,16 @@ typedef struct {
 } pyshmdata_readerObject;
 
 /*************/
-static PyObject* shmdata_writer_init(PyObject* self, PyObject* args);
+// Module
+static PyModuleDef pyshmdatamodule = {
+    PyModuleDef_HEAD_INIT,
+    "pyshmdata",
+    "Shmdata module",
+    -1,
+    NULL, NULL, NULL, NULL, NULL
+};
+
+PyMODINIT_FUNC PyInit_pyshmdata(void);
 
 #ifdef __cplusplush
 }
