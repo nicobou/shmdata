211b5ba899cc3d85379dba3dfe68f22473da23ae
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2014-10-21 16:20:18 -0400

    first tentative

---
+++ plugins/pjsip/build_pjproject.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd pjproject-2.2.1
+mkdir install_dir
+patch -p0 < ../more_codecs.patch
+./configure --prefix `pwd`/install_dir --disable-video  --disable-sound --disable-g711-codec --disable-g7221-codec  --disable-g722-codec --disable-gsm-codec --disable-ilbc-codec --disable-l16-codec --disable-speex-aec  --disable-speex-codec
+make dep
+make
+make install
+
