33bf0f14e2f20a7dee68b661a09b491bcca943d0
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2015-01-14 11:49:23 -0500

    adding args to abstract factory

--- switcher/abstract-factory.hpp
+++ switcher/abstract-factory.hpp
@@ -29,7 +29,7 @@
 #include "./creator.hpp"
 
 namespace switcher {
-template<class T, class Key, class Doc> class AbstractFactory {
+template<class T, class Key, class Doc, typename ...ATs> class AbstractFactory {
  public:
   template<class U> void register_class(Key Id, Doc doc);
   AbstractFactory();
@@ -37,22 +37,21 @@ template<class T, class Key, class Doc> class AbstractFactory {
 
   void register_class_with_custom_factory(Key Id,
                                           Doc doc,
-                                          T * (*custom_create) (),
+                                          T *(*custom_create) (ATs...),
                                           void(*custom_destroy) (T *));
   bool unregister_class(Key Id);
   std::vector<Key> get_keys();
   std::vector<Doc> get_classes_documentation();
   Doc get_class_documentation(Key Id);
-  std::shared_ptr<T> create(Key Id);
+  std::shared_ptr<T> create(Key Id, ATs... args);
   bool key_exists(Key Id);
 
  private:
-  std::map<Key, Creator < T> *>constructor_map_;
+  std::map<Key, Creator<T> *> constructor_map_;
   std::map<Key, void(*) (T *)> destructor_map_;
   std::map<Key, Doc> classes_documentation_;
 };
+
 }  // namespace switcher
-
 #include "./abstract-factory_spec.hpp"
-
 #endif
