fa469519706bdcbf9874522f373936b8da00a0dd
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2015-02-12 13:57:26 -0500

    adding a basic port checker

---
+++ switcher/port-checker.cpp
@@ -0,0 +1,65 @@
+/*
+ * This file is part of libswitcher.
+ *
+ * libswitcher is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string>
+#include "./port-checker.hpp"
+
+namespace switcher {
+
+bool PortChecker::is_used(std::uint16_t port){
+  bool res = false;
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(struct addrinfo));
+  hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+  hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+  hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+  hints.ai_protocol = 0;          /* Any protocol */
+  hints.ai_canonname = NULL;
+  hints.ai_addr = NULL;
+  hints.ai_next = NULL;
+  struct addrinfo *result;
+  int s = getaddrinfo(NULL, std::to_string(port).c_str(), &hints, &result);
+  if (s != 0) {
+    return false;
+  }
+  /* getaddrinfo() returns a list of address structures.
+     Try each address until we successfully bind(2).
+     If socket(2) (or bind(2)) fails, we (close the socket
+     and) try the next address. */
+  struct addrinfo *rp;
+  for (rp = result; rp != NULL; rp = rp->ai_next) {
+    int sfd = socket(rp->ai_family, rp->ai_socktype,
+                     rp->ai_protocol);
+    if (sfd == -1)
+      continue;
+    if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0) {
+      res = true;
+      break; /* Success */
+    }
+    close(sfd);
+  }
+  freeaddrinfo(result);
+  return res;
+}
+
+}  // namespace switcher
