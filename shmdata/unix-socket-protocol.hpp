9c48c2a4d637d6456fb87421aac1fe18c8f58a41
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2015-03-24 14:47:34 -0400

    adding missing files

---
+++ shmdata/unix-socket-protocol.hpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2015 Nicolas Bouillot (http://www.nicolasbouillot.net)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ */
+
+
+#ifndef _SHMDATA_UNIX_SOCKET_PROTOCOL_H_
+#define _SHMDATA_UNIX_SOCKET_PROTOCOL_H_
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <functional>
+
+namespace shmdata{
+
+// a basic initialisation and life monitoring protocol
+struct UnixSocketProtocol {
+  // connect/disconnect callbacks
+  using onPeerConnect = std::function<void(int id)>;
+  onPeerConnect on_connect_cb_{};
+  using onPeerDisconnect = std::function<void(int id)>;
+  onPeerDisconnect on_disconnect_cb_{};
+  // (server) get buffers to send back to clients when conecting
+  using iovServOnConnect = std::function<std::pair<const struct iovec *, size_t>()>;
+  iovServOnConnect get_connect_iov_{};
+  };
+
+struct onConnectData {
+  // data to distribute at connection
+  size_t shm_size_;
+  std::string shm_path_;
+  std::string user_data_;
+  // socket data structure pointing to previous members
+  const struct iovec iovec_[3];
+  // ctor
+  onConnectData(size_t shm_size,
+                const std::string &shm_path,
+                const std::string &user_data) :
+      shm_size_(shm_size),
+      shm_path_ (shm_path),
+      user_data_(user_data),
+      iovec_{
+    {&shm_size_, sizeof(size_t)},
+    {const_cast<char *>(shm_path_.c_str()), shm_path_.size()},
+    {const_cast<char *>(user_data_.c_str()), user_data_.size()}} {
+  }
+  onConnectData() = delete;
+};
+
+}  // namespace shmdata
+#endif
