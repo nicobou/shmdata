28c4e1053ce74c71fc8b52ed846c81d1d4db15dd
Emmanuel Durand <emmanueldurand@gmail.com>
2015-05-28 17:34:20 -0400

    PostureMerge converted to shmdata1

--- plugins/posture/posture_source.cpp
+++ plugins/posture/posture_source.cpp
@@ -74,7 +74,7 @@ PostureSrc::start() {
                               10000.0,
                               rgb_focal_,
                               (GParamFlags)
-                              G_PARAM_READWRITE,
+                              G_PARAM_READABLE,
                               PostureSrc::nope,
                               PostureSrc::get_rgb_focal,
                               this);
@@ -88,7 +88,7 @@ PostureSrc::start() {
                               10000.0,
                               depth_focal_,
                               (GParamFlags)
-                              G_PARAM_READWRITE,
+                              G_PARAM_READABLE,
                               PostureSrc::set_depth_focal,
                               PostureSrc::get_depth_focal,
                               this);
