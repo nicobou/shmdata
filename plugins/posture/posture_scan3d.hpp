88b8a106439fd4c874dec97b73488c4dd50463b9
Ludovic Schreiber <ludovic.schreiber@wanadoo.fr>
2015-06-04 10:04:29 -0400

    scan 3D with texture option

--- plugins/posture/posture_scan3d.hpp
+++ plugins/posture/posture_scan3d.hpp
@@ -38,27 +38,33 @@ class PostureSc3:public Quiddity, public StartableQuiddity {
   static void set_grid_props(const int resolution, void *user_data);
   static int get_reload_calibration(void *user_data);
   static void set_reload_calibration(const int reload, void *user_data);
+  static int get_colorize(void *user_data);
+  static void set_colorize(const int assert_colorize, void *user_data);
 
   static int get_output_mesh(std::vector<unsigned char>);
   void cb_frame_cloud(int index,
                       pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud);
+  void cb_frame_rgb(std::vector<unsigned char>& image, int width, int heigth);
 
   CustomPropertyHelper::ptr custom_props_;
   GParamSpec *nbr_props_ {nullptr};
-  GParamSpec *calibration_path_prop_ {nullptr};
-  GParamSpec *devices_path_prop_ {nullptr};
+  GParamSpec *calibration_path_props_ {nullptr};
+  GParamSpec *devices_path_props_ {nullptr};
   GParamSpec *grid_res_props_ {nullptr};
-  GParamSpec *reload_calibration_prop_ {nullptr};
+  GParamSpec *reload_calibration_props_ {nullptr};
+  GParamSpec *colorize_props_ {nullptr};
 
   std::vector<std::shared_ptr<posture::ZCamera>> cameras_ {};
   std::unique_ptr<posture::PointCloudMerger> merger_ {nullptr};
   std::unique_ptr<posture::Solidify> sol_ {nullptr};
+  std::unique_ptr<posture::Colorize> colorize_ {nullptr};
 
   std::vector<unsigned char> output_ {};
 
   mutable std::mutex mutex_;
 
   std::unique_ptr<ShmdataWriter> mesh_writer_ {nullptr};
+  std::unique_ptr<ShmdataWriter> rgb_writer_ {nullptr};
 
   int index_ {0};
   int nbr_ {1};
@@ -66,8 +72,12 @@ class PostureSc3:public Quiddity, public StartableQuiddity {
   std::string devices_path_ {"devices.xml"};
   int grid_res_ {3};
   bool reload_calibration_ {false};
+  bool colorize_or_not_ {false};
 
   bool is_started_ {false};
+
+  pcl::PolygonMesh::Ptr intermediate_mesh_ {nullptr};
+  std::vector<unsigned char> texture_;
 };
 
 SWITCHER_DECLARE_PLUGIN(PostureSc3);
