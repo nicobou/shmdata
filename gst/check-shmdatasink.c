1a1186ef0a375085162261cc2195dfb14a9c1f3f
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2015-06-26 17:13:48 -0400

    more into gst tests

--- gst/check-shmdatasink.c
+++ gst/check-shmdatasink.c
@@ -25,7 +25,9 @@
 #include "shmdata/clogger.h"
 #include "shmdata/cfollower.h"
 
-int server_interactions = 0;
+static GMainLoop *loop = NULL;
+static int server_interactions = 0;
+static res = 1;  // fail by default
 
 // *** follower callbacks
 void mylog(void *user_data, const char *str) {
@@ -44,28 +46,29 @@ void on_server_disconnect(void *user_data) {
 
 void on_data(void *user_data, void *data, size_t size) {
   printf("new data for client: size %zu ptr %p\n", size, data);
+  res = 0;
+  g_main_loop_quit(loop);
 }
 
 // *** gstreamer callbacks
-static gboolean
-bus_call (GstBus     *bus,
+static gboolean bus_call(GstBus     *bus,
           GstMessage *msg,
           gpointer    data)
 {
-  GMainLoop *loop = (GMainLoop *) data;
-  switch (GST_MESSAGE_TYPE (msg)) {
+  GMainLoop *loop =(GMainLoop *) data;
+  switch(GST_MESSAGE_TYPE(msg)) {
     case GST_MESSAGE_EOS:
-      g_print ("End of stream\n");
-      g_main_loop_quit (loop);
+      g_print("End of stream\n");
+      g_main_loop_quit(loop);
       break;
     case GST_MESSAGE_ERROR: {
       gchar  *debug;
       GError *error;
-      gst_message_parse_error (msg, &error, &debug);
-      g_free (debug);
-      g_printerr ("Error: %s\n", error->message);
-      g_error_free (error);
-      g_main_loop_quit (loop);
+      gst_message_parse_error(msg, &error, &debug);
+      g_free(debug);
+      g_printerr("Error: %s\n", error->message);
+      g_error_free(error);
+      g_main_loop_quit(loop);
       break;
     }
     default:
@@ -75,31 +78,28 @@ bus_call (GstBus     *bus,
 }
 
 // *** main
-
-int main () {
-return 0;  // FIXME
+int main() {
   //makeing a follower
-  ShmdataLogger logger = shmdata_make_logger(&mylog,
-                                             &mylog,
-                                             &mylog,
-                                             &mylog,
-                                             &mylog,
-                                             &mylog,
-                                             NULL);
-  assert(NULL != logger);
-  ShmdataFollower follower = shmdata_make_follower("/tmp/check-shmdatasink",
-                                                   &on_data,
-                                                   &on_server_connect,
-                                                   &on_server_disconnect,
-                                                   NULL,
-                                                   logger);
-  assert(NULL != follower);
+  ShmdataLogger logger = shmdata_make_logger(&mylog, 
+                                             &mylog, 
+                                             &mylog, 
+                                             &mylog, 
+                                             &mylog, 
+                                             &mylog, 
+                                             NULL); 
+  assert(NULL != logger); 
+  ShmdataFollower follower = shmdata_make_follower("/tmp/check-shmdatasink", 
+                                                   &on_data, 
+                                                   &on_server_connect, 
+                                                   &on_server_disconnect, 
+                                                   NULL, 
+                                                   logger); 
+  assert(NULL != follower); 
   //gstreamer shmdatasink for writing
-  GMainLoop *loop;
   GstElement *pipeline, *audiosource, *shmdatasink;
   GstBus *bus;
   guint bus_watch_id;
-  gst_init (NULL, NULL);
+  gst_init(NULL, NULL);
 #ifdef HAVE_CONFIG_H
   GstRegistry *registry = gst_registry_get();
   gst_registry_scan_path(registry, "./" LT_OBJDIR);
@@ -107,29 +107,29 @@ return 0;  // FIXME
   g_printerr("shmdatasink plugin not found");
 return -1;
 #endif
-  loop = g_main_loop_new (NULL, FALSE);
+  loop = g_main_loop_new(NULL, FALSE);
   /* Create gstreamer elements */
-  pipeline = gst_pipeline_new ("audio-player");
-  audiosource = gst_element_factory_make ("audiotestsrc", "audiosource");
-  shmdatasink = gst_element_factory_make ("shmdatasink", "shmdata-output");
+  pipeline = gst_pipeline_new("audio-player");
+  audiosource = gst_element_factory_make("audiotestsrc", "audiosource");
+  shmdatasink = gst_element_factory_make("shmdatasink", "shmdata-output");
   if (!pipeline || !audiosource || !shmdatasink) {
-    g_printerr ("One element could not be created. Exiting.\n"); return -1; }
-  g_object_set (G_OBJECT (shmdatasink), "socket-path", "/tmp/check-shmdatasink", NULL);
-  bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
-  bus_watch_id = gst_bus_add_watch (bus, bus_call, loop);
-  gst_object_unref (bus);
-  gst_bin_add_many (GST_BIN (pipeline), audiosource, shmdatasink, NULL);
-  gst_element_link (audiosource, shmdatasink);
-  gst_element_set_state (pipeline, GST_STATE_PLAYING);
-  g_main_loop_run (loop);
+    g_printerr("One element could not be created. Exiting.\n"); return -1; }
+  g_object_set(G_OBJECT(shmdatasink), "socket-path", "/tmp/check-shmdatasink", NULL);
+  bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
+  bus_watch_id = gst_bus_add_watch(bus, bus_call, loop);
+  gst_object_unref(bus);
+  gst_bin_add_many(GST_BIN(pipeline), audiosource, shmdatasink, NULL);
+  gst_element_link(audiosource, shmdatasink);
+  gst_element_set_state(pipeline, GST_STATE_PLAYING);
+  g_main_loop_run(loop);
   // cleaning gst
-  gst_element_set_state (pipeline, GST_STATE_NULL);
-  gst_object_unref (GST_OBJECT (pipeline));
-  g_source_remove (bus_watch_id);
-  g_main_loop_unref (loop);
-  // cleaning follower
-  shmdata_delete_follower(follower);
-  shmdata_delete_logger(logger);
-  return 0;
+  gst_element_set_state(pipeline, GST_STATE_NULL);
+  gst_object_unref(GST_OBJECT(pipeline));
+  g_source_remove(bus_watch_id);
+  g_main_loop_unref(loop);
+  // cleaning follower 
+  shmdata_delete_follower(follower); 
+  shmdata_delete_logger(logger); 
+  return res;
 }
 
--- gst/check-shmdatasrc.c
+++ gst/check-shmdatasrc.c
@@ -12,48 +12,83 @@
  * GNU Lesser General Public License for more details.
  */
 
-#include <assert.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include "shmdata/clogger.h"
-#include "shmdata/cwriter.h"
 
-int num_successive_write = 5;
+#ifdef HAVE_CONFIG_H
+#include "../config.h"
+#endif
 
-void mylog(void *user_data, const char *str) {
-  printf("%s\n", str);
+#include <gst/gst.h>
+#include <glib.h>
+
+static GMainLoop *loop = NULL;
+
+// *** gstreamer callbacks
+static gboolean bus_call(GstBus     *bus,
+          GstMessage *msg,
+          gpointer    data)
+{
+  GMainLoop *loop =(GMainLoop *) data;
+  switch(GST_MESSAGE_TYPE(msg)) {
+    case GST_MESSAGE_EOS:
+      g_print("End of stream\n");
+      g_main_loop_quit(loop);
+      break;
+    case GST_MESSAGE_ERROR: {
+      gchar  *debug;
+      GError *error;
+      gst_message_parse_error(msg, &error, &debug);
+      g_free(debug);
+      g_printerr("Error: %s\n", error->message);
+      g_error_free(error);
+      g_main_loop_quit(loop);
+      break;
+    }
+    default:
+      break;
+  }
+  return TRUE;
 }
 
 int main () {
-  ShmdataLogger logger = shmdata_make_logger(&mylog,
-                                             &mylog,
-                                             &mylog,
-                                             &mylog,
-                                             &mylog,
-                                             &mylog,
-                                             NULL);
-  assert(NULL != logger);
-  int i = num_successive_write;
-  while (-1 != --i) {
-    ShmdataWriter writer = shmdata_make_writer("/tmp/check-shmdatasrc",
-                                               sizeof(int),
-                                               "application/x-check-shmdata",
-                                               NULL,
-                                               NULL,
-                                               NULL,
-                                               logger);
-    assert(NULL != writer); 
-    usleep(50000);
-    int j = 10;
-    while (-1 != --j) {
-      // first option: copy
-      assert(0 != shmdata_copy_to_shm(writer, &j, sizeof(int)));
-    }
-    shmdata_delete_writer(writer);
-    usleep(50000);
-    }
-  shmdata_delete_logger(logger);
+  return 0;
+  //gstreamer shmdatasink for writing
+  GstElement *pipeline,
+      *audiosource, *shmdatasink,
+      *shmdatasrc, *fakesink;
+  GstBus *bus;
+  guint bus_watch_id;
+  gst_init(NULL, NULL);
+#ifdef HAVE_CONFIG_H
+  GstRegistry *registry = gst_registry_get();
+  gst_registry_scan_path(registry, "./" LT_OBJDIR);
+#else
+  g_printerr("shmdata plugins not found");
+return -1;
+#endif
+  loop = g_main_loop_new(NULL, FALSE);
+  /* Create gstreamer elements */
+  pipeline = gst_pipeline_new("audio-player");
+  audiosource = gst_element_factory_make("audiotestsrc", "audiosource");
+  shmdatasink = gst_element_factory_make("shmdatasink", "shmdata-output");
+  shmdatasrc = gst_element_factory_make("shmdatasrc", "shmdata-input");
+  fakesink = gst_element_factory_make("fakesink", "fake");
+  if (!pipeline || !audiosource || !shmdatasink || !shmdatasrc || !fakesink) {
+    g_printerr("One element could not be created. Exiting.\n"); return -1; }
+  g_object_set(G_OBJECT(shmdatasink), "socket-path", "/tmp/check-shmdatasrc", NULL);
+  g_object_set(G_OBJECT(shmdatasrc), "socket-path", "/tmp/check-shmdatasrc", NULL);
+  bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
+  bus_watch_id = gst_bus_add_watch(bus, bus_call, loop);
+  gst_object_unref(bus);
+  gst_bin_add_many(GST_BIN(pipeline), audiosource, shmdatasink, shmdatasrc, fakesink, NULL);
+  gst_element_link(audiosource, shmdatasink);
+  gst_element_link(shmdatasrc, fakesink);
+  gst_element_set_state(pipeline, GST_STATE_PLAYING);
+  g_main_loop_run(loop);
+  // cleaning gst
+  gst_element_set_state(pipeline, GST_STATE_NULL);
+  gst_object_unref(GST_OBJECT(pipeline));
+  g_source_remove(bus_watch_id);
+  g_main_loop_unref(loop);
   return 0;
 }
 
