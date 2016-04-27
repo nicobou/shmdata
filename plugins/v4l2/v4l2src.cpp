78fd37deaa88e4b551f2eb54ebaf3eb8d5b3d465
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2016-04-27 13:52:04 -0400

    defining default_initial_shmsize_

--- plugins/v4l2/v4l2src.cpp
+++ plugins/v4l2/v4l2src.cpp
@@ -50,9 +50,7 @@ void V4L2Src::set_shm_suffix(){
     shmpath_ = make_file_name(enc_suffix_);
   g_object_set(G_OBJECT(shmsink_.get_raw()),
                "socket-path", shmpath_.c_str(),
-               // set large initial size in order to avoid automatic resize
-               // for first frame, 67108864 is enough for 4096x4096 RGBA
-               "initial-size", 67108864,
+               "initial-size", default_initial_shmsize_,
                nullptr);
 }
 
