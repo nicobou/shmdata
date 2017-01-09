bc9988872175e35d8a8d69c9167062a728312b0d
Jérémie Soria <jsoria@sat.qc.ca>
2017-01-09 14:18:51 -0500

    Prevents from rebuilding docker image each time.

--- ci/install_shmdata.sh
+++ ci/install_shmdata.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-shmdata_branch = "develop"
-
-if [[ "$1" = "master" ]]; then
-    shmdata_branch = "master"
-fi
-
 echo SHMDATA BUILD
 git clone https://github.com/sat-metalab/shmdata.git libshmdata
 cd libshmdata
-git checkout ${shmdata_branch}
+git checkout $1
 mkdir build
 cd build
 cmake ..
