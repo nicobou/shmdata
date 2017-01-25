7497b3e9d64341fd1a4254913e19dcb1ff000871
Jérémie Soria <jsoria@sat.qc.ca>
2017-01-25 15:03:10 -0500

    Added support for multichannel in ladspa plugin.

--- plugins/ladspa/ladspa.hpp
+++ plugins/ladspa/ladspa.hpp
@@ -22,6 +22,7 @@
 #include "switcher/shmdata-connector.hpp"
 #include "switcher/shmdata-follower.hpp"
 #include "switcher/shmdata-writer.hpp"
+#include "switcher/threaded-wrapper.hpp"
 
 namespace switcher {
 /**
@@ -57,11 +58,18 @@ class LADSPA : public Quiddity {
   //! Gstreamer pipeline creation methods
   bool create_gst_pipeline();
   void get_gst_properties();
+  void mirror_gst_properties();
 
   //! Shmdata methods
   bool on_shmdata_connect(const std::string& shmpath);
+  void create_and_play_gst_pipeline();
   bool on_shmdata_disconnect();
   bool can_sink_caps(std::string str_caps);
+
+  // Property custom saving
+  InfoTree::ptr on_saving() final;
+  void on_loading(InfoTree::ptr&& tree) final;
+  void save_properties();
 
   bool is_valid_{false};   //!< Used to validate that the construction of the quiddity worked
   std::string shmpath_{};  //!< Path of the input shmdata
@@ -72,17 +80,32 @@ class LADSPA : public Quiddity {
   std::unique_ptr<GstShmdataSubscriber> shmsink_sub_{nullptr};  //!< Subscriber to output shmdata
   bool first_connect_{false};  //!< First connection to a LADSPA plugin?
 
-  GstElement* shmdatasrc_{nullptr};      //!< Gstreamer shmdatasrc element
-  GstElement* shmdatasink_{nullptr};     //!< Gstreamer shmdatasink element
-  GstElement* ladspa_element_{nullptr};  //!< Gstreamer specific ladspa element
-  GstElement* ladspa_bin_{nullptr};      //!< Full ladspa pipeline
+  GstElement* shmdatasrc_{nullptr};             //!< Gstreamer shmdatasrc element
+  GstElement* shmdatasink_{nullptr};            //!< Gstreamer shmdatasink element
+  std::vector<GstElement*> ladspa_elements_{};  //!< Gstreamer specific ladspa element
+  GstElement* ladspa_bin_{nullptr};             //!< Full ladspa pipeline
 
   std::vector<std::string> properties_;  //!< Gstreamer properties of the currently selected plugin
-  std::map<std::string, std::string>
-      saved_properties_;  //!< Properties values for the selected plugin
+  std::map<std::string, std::map<std::string, std::string>>
+      saved_properties_;  //!< Properties values for the selected plugin for each ladspa instance
 
+  bool reset_saved_properties_{false};  //!< Used to keep saved properties in some cases
+
+  int channels_number_{1};
+  PContainer::prop_id_t global_group_id_{0};      //!< Group containing global properties
+  PContainer::prop_id_t perchannel_group_id_{0};  //!< Group containing per-channel properties
   PluginList plugins_list_;           //!< List of all LADSPA plugins available on the system
   Selection<> plugins_;               //!< Plugin list property data
   PContainer::prop_id_t plugins_id_;  //!< Plugins list property id
+  bool global_settings_{true};  //!< Indicates if global properties must be propagated per channel
+  PContainer::prop_id_t global_settings_id_{0};  //!< Property id of global properties boolean
+  std::unique_ptr<ThreadedWrapper<>> channels_change_th_{
+      std::make_unique<ThreadedWrapper<>>()};  //!< Threaded wrapper used for dynamic reconstruction
+                                               //! of pipeline when channels number changes
+  std::mutex channels_change_mutex_{};  //!< Used when channels number changes and reconstruction of
+                                        //! pipeline
+  std::vector<std::pair<PContainer::prop_id_t, PContainer::register_id_t>>
+      prop_subscribers_{};  //!< Subscribers to global properties in order to propagate their values
+                            //! to per-channel properties
 };
 };
