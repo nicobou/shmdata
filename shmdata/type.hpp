a299b0de5a9ca31af0fbff5c5a4246b5eb7f266e
Nicolas Bouillot <nbouillot@sat.qc.ca>
2019-01-14 16:20:54 -0500

    adding custom type names in type parser

--- shmdata/type.hpp
+++ shmdata/type.hpp
@@ -19,22 +19,38 @@
 namespace shmdata {
 
 class Type {
+  template <typename T>
+  struct empty_t {
+    using type = T;
+  };
+
  public:
   Type(const std::string& type);
   std::string name() const;
   std::any get(const std::string& key) const;
-  std::map<std::string,std::any> get_properties() const;
+  std::map<std::string, std::any> get_properties() const;
   std::string str() const;
   std::string get_parsing_errors() const;
   std::string get_serialization_errors() const;
-  template <typename T> void set_prop(const std::string& key,const T& value) {
+  template <typename T>
+  void set_prop(const std::string& key, const T& value) {
     properties_.emplace(key, value);
   }
+  // set a property with a custom type name:
+  void set_prop(const std::string& key, const std::string& custom_type, const std::string& value);
+  // overload that allows for passing const char* instead of always std::string
+  void set_prop(const std::string& key, const char* value);
+
  private:
   static std::string escape(const std::string& str);
   static std::string unescape(const std::string& str);
+  static std::string get_serialized_string_value(const std::any& value);
   std::string name_{};
-  std::map<std::string,std::any> properties_{};
+  std::map<std::string, std::any> properties_{};
+  // The str_type member stores type names asssociated to keys.
+  // For instance, the pair <"framerate", "fraction"> specify the "framerate" key
+  // will be serialized with the type "fraction"
+  std::map<std::string, std::string> str_types_{};
   std::string parsing_errors_{};
   mutable std::string serialization_errors_{};
 };
--- tests/check-type-parser.cpp
+++ tests/check-type-parser.cpp
@@ -23,6 +23,10 @@ bool check_value(const shmdata::Type& type, const std::string& key, const T& val
   return std::any_cast<T>(type.get(key)) == value;
 }
 
+bool check_value(const shmdata::Type& type, const std::string& key, const char* value) {
+  return std::any_cast<std::string>(type.get(key)) == value;
+}
+
 int main () {
   using namespace shmdata;
 
@@ -30,27 +34,39 @@ int main () {
     auto test_type_str = std::string(
         "video/x-raw, format=(string)I420, width=(int)320, height=(int)240, "
         "framerate=(fraction)30/1, multiview-mode=(string)mono, pixel-aspect-ratio=(fraction)1/1, "
-        "interlace-mode=(string)progressive  , label=(string)\"unexpected\\, commas\\,\"");
+        "interlace-mode=(string)progressive  , label=(string)\"\\(unexpected\\) \\= chars\\,\"");
     auto type = Type(test_type_str);
     assert(type.name() == "video/x-raw");
     assert(type.get_properties().size() == 8);
-    assert(check_value<std::string>(type, "label", "unexpected, commas,"));
+    std::cout << std::any_cast<std::string>(type.get("label")) << '\n';
+    assert(check_value<std::string>(type, "label", "(unexpected) = chars,"));
     assert(check_value<std::string>(type, "pixel-aspect-ratio", "1/1"));
     assert(check_value<int>(type, "width", 320));
   }
 
   {  // check building a Type and consistency when serializing/deserializing it
     auto type = Type("video/x-raw");
-    type.set_prop("width", 4096);
+    type.set_prop("format", "I420");
+    type.set_prop("framerate", "fraction", "30/1");
     type.set_prop("height", 4096);
-    type.set_prop("label", std::string("\"unexpected\\, commas\\,\""));
-    // building an other Type from 'type' serialization and check it has the right name and properties 
-    std::cout << type.str() << '\n';
+    type.set_prop("interlace-mode", "progressive");
+    type.set_prop("label", "unexpected = comma,");
+    type.set_prop("multiview-mode", "mono");
+    type.set_prop("pixel-aspect-ratio", "fraction", "1/1");
+    type.set_prop("width", 4096);
+    // building an other Type from 'type' serialization and check it has the right name and
     auto type2 = Type(type.str());
     assert(type2.name() == "video/x-raw");
-    assert(check_value(type2, "width", 4096));
+    assert(check_value(type2, "format", "I420"));
+    assert(check_value(type2, "framerate", "30/1"));
     assert(check_value(type2, "height", 4096));
-    assert(check_value(type2, "label", std::string("unexpected, commas,")));
+    assert(check_value(type2, "interlace-mode", "progressive"));
+    assert(check_value(type2, "label", "unexpected = comma,"));
+    assert(check_value(type2, "multiview-mode", "mono"));
+    assert(check_value(type2, "pixel-aspect-ratio", "1/1"));
+    assert(check_value(type2, "width", 4096));
+    // check serializations are the same
+    assert(type.str() == type2.str());
   }
   return 0;
 }
