296a48f3856fd388eedc1d40998258a54b80b442
Nicolas Bouillot <nicolas.bouillot@gmail.com>
2021-02-09 15:35:04 -0500

    🐛 fix shmdata2jack not attaching to the jack dummy server

--- tools/tests/check_shmdata2jack.sh
+++ tools/tests/check_shmdata2jack.sh
@@ -3,17 +3,19 @@
 # the script will fail if one command fails
 set -e
 
-jackd --no-realtime --name check_shmdata2jack -d dummy -r 48000 &
-jackserv=$!
+# run dummy jack in background 
+jackd --no-realtime --name check_shmdata2jack -d dummy -r 48000 & jackserv=$!
 
-gst-launch-1.0 --gst-plugin-path=/usr/local/lib/gstreamer-1.0/ audiotestsrc ! shmdatasink socket-path=/tmp/check_shmdata2jack &
-gstpipe=$!
+# produce an audio shmdata in background
+gst-launch-1.0 --gst-plugin-path=/usr/local/lib/gstreamer-1.0/ audiotestsrc ! shmdatasink socket-path=/tmp/check_shmdata2jack & gstpipe=$!
 
- JACK_DEFAULT_SERVER=check_shmdata2jack shmdata2jack -v -n check_shmdata2jack  --connect-all-to-first /tmp/check_shmdata2jack &
-shmdata2jack=$!
+# run shmdata2jack
+shmdata2jack -v -s check_shmdata2jack -n check_shmdata2jack_client  --connect-all-to-first /tmp/check_shmdata2jack & shmdata2jack=$!
 
+# wait
 sleep 1
 
+# kill processes
 kill -s SIGTERM $shmdata2jack
 kill -s SIGTERM $gstpipe
 kill -s SIGTERM $jackserv
