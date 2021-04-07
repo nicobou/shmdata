b07a0df42f9df63f807eca73313139142b504334
Emmanuel Durand <emmanueldurand@gmail.com>
2021-04-07 10:57:04 -0400

    ✨ Added framerate and frame duration counters

--- utils/sdflow.cpp
+++ utils/sdflow.cpp
@@ -10,15 +10,23 @@
  * GNU Lesser General Public License for more details.
  */
 
-#include <unistd.h>
 #include <signal.h>
-#include <memory>
-#include <iostream>
-#include <thread>
-#include <algorithm>
+#include <unistd.h>
+
+#include <chrono>
 #include <climits>
-#include "shmdata/follower.hpp"
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <numeric>
+#include <thread>
+
 #include "shmdata/console-logger.hpp"
+#include "shmdata/follower.hpp"
+
+#define MAX_FRAME_TIME_COUNT 30
 
 using namespace shmdata;
 
@@ -30,21 +38,25 @@ void leave(int sig) {
 }
 
 void usage(const char *prog_name){
-  printf("usage: %s [-d] shmpath\n", prog_name);
+  printf("usage: %s [-d] [-f] shmpath\n", prog_name);
   exit(1);
 }
 
 int main (int argc, char *argv[]) {
   bool debug = false;
+  bool show_frame_timings = false;
   char *shmpath = nullptr;
 
   opterr = 0;
   int c = 0;
-  while ((c = getopt (argc, argv, "d")) != -1)
+  while ((c = getopt (argc, argv, "df")) != -1)
     switch (c)
       {
         case 'd':
           debug = true;
+          break;
+        case 'f':
+          show_frame_timings = true;
           break;
         case '?':
           break;
@@ -67,11 +79,36 @@ int main (int argc, char *argv[]) {
   logger.set_debug(debug);
   unsigned long long int frame_count = 0;
   {
+    std::deque<int64_t> frames_time;
+
     follower.reset(
         new Follower(shmpath,
-                     [&frame_count](void *data, size_t size){
-                       std::cout << frame_count 
-                                 << "    size: " << size
+                     [&frame_count, &frames_time, show_frame_timings](void *data, size_t size){
+                       float frame_duration = 0.f;
+                       float framerate = 0.f;
+                       if (show_frame_timings) {
+                         int64_t current_frame_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+                         frames_time.push_back(current_frame_time);
+
+                         while (frames_time.size() > MAX_FRAME_TIME_COUNT)
+                           frames_time.pop_front();
+
+                         if (frames_time.size() > 1) {
+                           frame_duration = frames_time.back() - frames_time[frames_time.size() - 2];
+                           int64_t total_frames_duration = frames_time.back() - frames_time.front();
+                           float mean_time_per_frame = static_cast<float>(total_frames_duration) / static_cast<float>(frames_time.size() - 1);
+                           framerate = 1000.f / mean_time_per_frame;
+                         }
+                       }
+
+                       std::cout << frame_count;
+
+                       if (show_frame_timings) {
+                       std::cout << std::setprecision(0) << std::fixed << "    dT: " << frame_duration << "ms"
+                                 << std::setprecision(2) << std::fixed << "    fps: " << framerate;
+                       }
+
+                       std::cout << "    size: " << size
                                  << "    data: ";
                        if (ULLONG_MAX == frame_count)
                          frame_count = 0;
